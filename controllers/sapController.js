914c5d93eaaa363b8972ca8e3f00dc792f11e573
ikbs-rs <116352655+ikbs-rs@users.noreply.github.com>
2025-05-19 01:02:22 +0200

    ...

--- controllers/sapController.js
+++ controllers/sapController.js
@@ -2,7 +2,14 @@ import { fetchSapSessionAndToken } from '../utils/sapClient.js';
 
 export const getSapSessionAndToken = async (req, res) => {
   try {
-    const result = await fetchSapSessionAndToken();
+
+    const { username, password } = req.user;
+    
+    if (!username || !password) {
+      return res.status(400).json({ message: 'Korisnički podaci nisu pronađeni u tokenu' });
+    }
+
+    const result = await fetchSapSessionAndToken(username, password);
     res.json(result);
   } catch (err) {
     res.status(500).json({ message: err.message });
--- utils/sapClient.js
+++ utils/sapClient.js
@@ -1,11 +1,11 @@
 import axios from 'axios';
 import dotenv from 'dotenv';
 
-export const fetchSapSessionAndToken = async () => {
+export const fetchSapSessionAndToken = async (username, password) => {
   // const sapUrl = `http://emd-s4h.ems.local:8001/sap/bc/zhr_pa2001?sap-client=100`;
   const sapUrl = `${process.env.SAP_WS}/zhr_pa2001?sap-client=${process.env.SAP_CLIENT}`;
-  const sapUsername = 'bvasiljevic';
-  const sapPassword = 'test4321';
+  const sapUsername = username;
+  const sapPassword = password;
 
 // console.log("Dosao u SAP client")
   const initial = await axios.get(sapUrl, {
